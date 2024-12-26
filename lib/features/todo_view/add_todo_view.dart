41a9484a65866a8e69cd34f63b1ace930d46dfc4
loppiko <insertthemontana@gmail.com>
2024-12-26 21:06:57 +0100

    renaming files

---
+++ lib/features/todo_view/add_todo_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AddTodoScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("AddTodoScreen"),
+      ),
+    );
+  }
+}
